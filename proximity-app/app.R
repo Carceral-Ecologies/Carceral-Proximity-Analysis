0b2a16ca793b6f0890f8617cc195b41516757b02
Lindsay Poirier <lindsay.poirier13@gmail.com>
2020-06-15 13:31:29 -0700

    Moved military base data cleaning into separate script and replaced data inputs

--- proximity-app/app.R
+++ proximity-app/app.R
@@ -26,35 +26,9 @@ pb_crs <- st_crs(pb_sf) #get the CRS for prison centroids
 #Until we verify census tract join, importing prison file with census tracts separately. Eventually will replace code above to only import this file. 
 pb_with_census_tracts <- read.csv("prisons_with_census_tracts.csv", stringsAsFactors = FALSE)
 
-#Read two military site shapefiles
-mil <- st_read("installations_ranges/MILITARY_INSTALLATIONS_RANGES_TRAINING_AREAS_PT.shp", stringsAsFactors = FALSE)
-mil2 <- st_read("installations_ranges/MILITARY_INSTALLATIONS_RANGES_TRAINING_AREAS_BND.shp", stringsAsFactors = FALSE)
-
-#Bind the two military site shapefiles
-mil <- rbind(mil, mil2)
-
-#Convert military sites to match (larger) FRS fac data set Coordinate Reference System
-mil_sf <- st_transform(mil, crs = 4269)
-
-#Reduce military sites from polygons to points (centroids) to reduces distance calculation times
-mil_sf <- st_transform(mil_sf, crs = 32617) %>% #convert to utm for calculating centroids
-  st_centroid() %>% #centroids from original multipolygons
-  st_transform(crs = 4269) #back to 4269
-
-#state_codes.csv contains full state names, state abbreviations, and census codes for each state, allowing conversion between the variables in each data file
-    #Ben: added ' ' to fix Windows column name issue from this .csv, caused by 'Byte Order Mark'
-    #per https://stackoverflow.com/questions/24568056/rs-read-csv-prepending-1st-column-name-with-junk-text/24568505
-codes <- read.csv("state_codes.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE) 
-codes$STATE_NUM <- as.character(codes$STATE_NUM) %>% #convert state census codes to characters because they will eventually be text in file names
-  str_pad(2, pad = "0") #add a leading zero to ensure all state numbers are two digits
-
-#There are census tract shapefiles for each state stored in the project files. To reduce reading times, these are not all read in by default but on a state-by-state basis when a user selects a state. Below we add two columns with the path to the zip of each state's shapefile, along with the file itself.
-codes <- codes %>%
-  mutate(TRACT_ZIP = paste("tracts/tl_2019_", STATE_NUM, "_tract.zip", sep = "")) %>% #create new column in codes dataframe with location of each census tract shapfile zip directory by filling the state census codes into the path
-  mutate(TRACT_FOLDER = paste("tracts/tl_2019_", STATE_NUM, "_tract/", sep = "")) %>% #create new column in codes dataframe with location of each census tract shapfile zip directory by filling the state census codes into the path
-  mutate(TRACT_FILE = paste("tl_2019_", STATE_NUM, "_tract.shp", sep = "")) #create new column in codes dataframe with location of each census tract shapefile name
-
-mil_sf <- mil_sf %>% left_join(codes, by = "STATE_TERR") #Add the state abbreviation to military site data frame, which only contains the fill state name.
+#Read military bases data file
+mil <- st_read("military_bases.csv", stringsAsFactors = FALSE) 
+mil_sf <- st_as_sf(mil, coords = c("X", "Y"), crs = pb_crs, na.fail = FALSE)
 
 #Read brownfield data file and convert to sf
 bf <- read.csv("brownfields.csv", stringsAsFactors = FALSE)
@@ -73,6 +47,19 @@ ap_sf <- st_as_sf(ap, coords = c("X", "Y"), crs = pb_crs, na.fail = FALSE)
 #Read superfund sites data file and convert to sf
 sfs <- read.csv("sf.csv", stringsAsFactors = FALSE) 
 sfs_sf <- st_as_sf(sfs, coords = c("LONGITUDE83", "LATITUDE83"), crs = pb_crs, na.fail = FALSE)
+
+#state_codes.csv contains full state names, state abbreviations, and census codes for each state, allowing conversion between the variables in each data file
+#Ben: added ' ' to fix Windows column name issue from this .csv, caused by 'Byte Order Mark'
+#per https://stackoverflow.com/questions/24568056/rs-read-csv-prepending-1st-column-name-with-junk-text/24568505
+codes <- read.csv("state_codes.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE) 
+codes$STATE_NUM <- as.character(codes$STATE_NUM) %>% #convert state census codes to characters because they will eventually be text in file names
+  str_pad(2, pad = "0") #add a leading zero to ensure all state numbers are two digits
+
+#There are census tract shapefiles for each state stored in the project files. To reduce reading times, these are not all read in by default but on a state-by-state basis when a user selects a state. Below we add two columns with the path to the zip of each state's shapefile, along with the file itself.
+codes <- codes %>%
+  mutate(TRACT_ZIP = paste("tracts/tl_2019_", STATE_NUM, "_tract.zip", sep = "")) %>% #create new column in codes dataframe with location of each census tract shapfile zip directory by filling the state census codes into the path
+  mutate(TRACT_FOLDER = paste("tracts/tl_2019_", STATE_NUM, "_tract/", sep = "")) %>% #create new column in codes dataframe with location of each census tract shapfile zip directory by filling the state census codes into the path
+  mutate(TRACT_FILE = paste("tl_2019_", STATE_NUM, "_tract.shp", sep = "")) #create new column in codes dataframe with location of each census tract shapefile name
 
 ui <- navbarPage("Proximity Analysis", id="nav",
                  
@@ -220,7 +207,11 @@ ui <- navbarPage("Proximity Analysis", id="nav",
                                          infoBoxOutput("total_capacity_pb_bf", width = 3), #display total capacity across prisons with brownfield in census tract
                                          infoBoxOutput("percent_capacity_pb_bf", width = 3), #display percent of capacity across prisons with brownfield in census tract
                                          infoBoxOutput("missing_capacity", width = 3), #display number of prisons with missing capacity
-                                         tabsetPanel(
+                                         #infoBoxOutput("num_census_pb", width = 3),
+                                         #infoBoxOutput("num_census_bf", width = 3),
+                                         #infoBoxOutput("num_census", width = 3),
+                                         #infoBoxOutput("num_census_pb_bf", width = 3),
+                                         
                                            tabPanel("Plot", 
                                                     box(
                                                       plotOutput("pb_bf_frequency"), width = 12 #plot the distribution of census tract brownfields counts across prisons
@@ -234,7 +225,7 @@ ui <- navbarPage("Proximity Analysis", id="nav",
                                      )
                                    )
                           )
-)
+
 
 server <- function(input, output, session) {
   
@@ -620,6 +611,50 @@ server <- function(input, output, session) {
     
     infoBox('Percent of prisons with missing capacity', missing_capacity_rows, color = "yellow")
   })
+  
+  output$num_census_bf <- renderInfoBox({
+    census_bf_rows <- bf_with_census_tracts %>%
+      count(GEOID) %>%
+      filter(n > 1) %>%
+      nrow()
+    
+    infoBox('Number of census tracts with 2 brownfields', census_bf_rows, color = "yellow")
+  })
+  
+  output$num_census_pb <- renderInfoBox({
+    census_pb_rows <- pb_with_census_tracts %>%
+      count(GEOID) %>%
+      filter(n > 0) %>%
+      nrow()
+    
+    infoBox('Number of census tracts with a prison', census_pb_rows, color = "yellow")
+  })
+  
+  output$num_census_pb_bf <- renderInfoBox({
+    census_pb_bf <- census_tract_data %>%
+      filter(!is.na(BF_COUNT) & BF_COUNT > 0 & !is.na(PB_COUNT)) %>%
+      nrow()
+     
+    infoBox('Number of census tracts with a prison and 2 brownfields', census_pb_bf, color = "yellow")
+  })
+  
+  output$num_census <- renderInfoBox({
+    census_bf_rows <- bf_with_census_tracts %>%
+      count(GEOID) %>%
+      filter(n > 0) %>%
+      nrow()
+    
+    census_pb_bf <- census_tract_data %>%
+      filter(!is.na(BF_COUNT) & BF_COUNT > 0 & !is.na(PB_COUNT)) %>%
+      nrow()
+    
+    census_rows <-  census_pb_bf / census_bf_rows * 100
+    
+    
+    infoBox('Percentage of census tracts with two brownfields that also have a prison', census_rows, color = "yellow")
+  })
+  
+  
   
   #Data table of prisons with brownfields in census tract, sorted according to number of brownfields
   output$pb_most_bf <- DT::renderDataTable(
